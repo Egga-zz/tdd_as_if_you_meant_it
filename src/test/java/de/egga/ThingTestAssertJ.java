8196c9a99bee08f277778c46f442bbc168a55b59
Egga (Jan Hartung) <github@egga.de>
2016-04-30 16:20:11 +0200

    Living cell dies with one or two neighbours.

--- src/test/java/de/egga/ThingTestAssertJ.java
+++ src/test/java/de/egga/ThingTestAssertJ.java
@@ -7,9 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ThingTestAssertJ {
 
     @Test
-    public void it_should_call_for_action() {
+    public void cell_should_die_with_only_one_neighbor() {
+        int numberOfNeighbors = 1;
+        String newState = numberOfNeighbors == 1 ? "dead" : null;
+        assertThat(newState).isEqualTo("dead");
+    }
 
-
-        assertThat("Done.").isEqualTo("Done.");
+    @Test
+    public void cell_should_die_with_only_two_neighbors() {
+        int numberOfNeighbors = 2;
+        String newState = numberOfNeighbors == 2 ? "dead" : null;
+        assertThat(newState).isEqualTo("dead");
     }
 }
