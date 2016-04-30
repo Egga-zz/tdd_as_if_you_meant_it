2e844db174954d297d595bbdf325e4845000eeaf
Egga (Jan Hartung) <github@egga.de>
2016-04-30 16:13:25 +0200

    asd

--- src/test/java/de/egga/ThingTestAssertJ.java
+++ src/test/java/de/egga/ThingTestAssertJ.java
@@ -8,8 +8,8 @@ public class ThingTestAssertJ {
 
     @Test
     public void it_should_call_for_action() {
-        Thing thing = new Thing();
-        String value = thing.callForAction();
-        assertThat(value).isEqualTo("Done.");
+
+
+        assertThat("Done.").isEqualTo("Done.");
     }
 }
