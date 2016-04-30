fbf7ee5d3603f7d46a5c80630a8bc200556b6663
Egga (Jan Hartung) <github@egga.de>
2016-04-30 16:30:08 +0200

    Extract second code block.

--- src/test/java/de/egga/ThingTestAssertJ.java
+++ src/test/java/de/egga/ThingTestAssertJ.java
@@ -9,34 +9,39 @@ public class ThingTestAssertJ {
     public static final String DEAD = "dead";
     public static final String ALIVE = "alive";
 
-    private String newCellStateFor(int numberOfNeighbors) {
+    private String livingCellWillBeDeadWith(int numberOfNeighbors) {
         return numberOfNeighbors == 0 || numberOfNeighbors == 1 ? DEAD : null;
+    }
+
+    private String livingCellWillBeAliveWith(int numberOfNeighbors) {
+        return numberOfNeighbors == 2 || numberOfNeighbors == 3 ? ALIVE : null;
     }
 
     @Test
     public void cell_should_die_with_no_neighbors() {
         int numberOfNeighbors = 0;
-        String newState = newCellStateFor(numberOfNeighbors);
+        String newState = livingCellWillBeDeadWith(numberOfNeighbors);
         assertThat(newState).isEqualTo(DEAD);
     }
 
     @Test
     public void cell_should_die_with_only_one_neighbor() {
         int numberOfNeighbors = 1;
-        String newState = newCellStateFor(numberOfNeighbors);
+        String newState = livingCellWillBeDeadWith(numberOfNeighbors);
         assertThat(newState).isEqualTo(DEAD);
     }
 
     @Test
     public void cell_should_live_with_two_neighbors() {
         int numberOfNeighbors = 2;
-        String newState = numberOfNeighbors == 2 || numberOfNeighbors == 3 ? ALIVE : null;
+        String newState = livingCellWillBeAliveWith(numberOfNeighbors);
         assertThat(newState).isEqualTo(ALIVE);
     }
+
     @Test
     public void cell_should_live_with_three_neighbors() {
         int numberOfNeighbors = 3;
-        String newState = numberOfNeighbors == 2 || numberOfNeighbors == 3 ? ALIVE : null;
+        String newState = livingCellWillBeAliveWith(numberOfNeighbors);
         assertThat(newState).isEqualTo(ALIVE);
     }
 }
